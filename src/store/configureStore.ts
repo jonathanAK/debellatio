dda3a1d3a69a5b11298697cdbdaf552eb76ff206
jonathanak <jonathan@amitkohn.com>
2019-08-07 16:53:59 +0300

    Lodash update

---
+++ src/store/configureStore.ts
@@ -0,0 +1,10 @@
+import {combineReducers, createStore} from 'redux'
+import territoriesDataReducer from './territoriesDataReducer';
+
+export default function configureStore() {
+    // combine all reducers to create  root reducer
+    const rootReducer = combineReducers({
+        territories:territoriesDataReducer
+    });
+    return createStore(rootReducer);
+}
