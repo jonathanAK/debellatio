198c4580d7c7a95640e329597cee01855acdc90b
jonathanak <jonathan@amitkohn.com>
2019-09-10 18:14:35 +0300

    first test take 3

--- client/src/store/configureStore.ts
+++ client/src/store/configureStore.ts
@@ -1,11 +1,12 @@
 import {combineReducers, createStore, applyMiddleware} from 'redux'
 import viewsReducer from "./views.reducer";
-import socketMiddleware from "./socketMiddleware";
+// import socketMiddleware from "./socketMiddleware";
 
 export default function configureStore() {
     // combine all reducers to create  root reducer
     const rootReducer = combineReducers({
         views:viewsReducer
     });
-    return createStore(rootReducer,applyMiddleware(socketMiddleware()));
+    return createStore(rootReducer);
+    // return createStore(rootReducer,applyMiddleware(socketMiddleware()));
 }
