ee4719d706cf838d4127bcc0329228ca33eeb5fb
jonathanak <jonathan@amitkohn.com>
2019-09-20 17:49:00 +0300

    cleaned up socket middleware

--- client/src/store/socketMiddleware.ts
+++ client/src/store/socketMiddleware.ts
@@ -1,10 +1,7 @@
 import openSocket from 'socket.io-client';
 import {Action} from '../models/Action';
 import {Dispatch, Middleware, MiddlewareAPI} from 'redux';
-import {debellatioSetView} from "./views.reducer";
-import {ActiveViewEnum} from "../models/ActiveView";
-import {debellatioSetGameCode, debellatioSetPlayers} from "./misc.reducer";
-
+import socketListners from "./socketListners";
 
 //define middleware action
 const DEBELATIO_SEND_SOCKET = "DEBELATIO_SEND_WEBSOCKET_MESSAGE";
@@ -22,15 +19,8 @@ const socketMiddleware:Middleware = ({dispatch}: MiddlewareAPI)=>{
     // const  socket = openSocket(process.env.PUBLIC_URL);
 
     //register socket listeners
-    socket.on('gameCreated', (message:string) => {
-        dispatch(debellatioSetView (ActiveViewEnum.WaitingForPlayers));
-        dispatch(debellatioSetGameCode(message));
-    });
-    socket.on('gameJoined', () => {
-        dispatch(debellatioSetView (ActiveViewEnum.WaitingForPlayers));
-    });
-    socket.on('playerJoined', (message:[object]) => {
-        dispatch(debellatioSetPlayers(message));
+    socketListners(dispatch).forEach(socketListner=>{
+        socket.on(socketListner.event, socketListner.fn);
     });
 
     //handle sending socket messages to server
