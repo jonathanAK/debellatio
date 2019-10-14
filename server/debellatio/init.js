06b06c616334611006b6ab108fbbe7179e93a04f
jonathanak <jonathan@amitkohn.com>
2019-10-14 11:19:31 +0300

    settings sent to server

--- server/debellatio/init.js
+++ server/debellatio/init.js
@@ -4,10 +4,10 @@ module.exports.initGameSettings =(msg,userId)=>{
     const gameId =  uniqueGameCode();
     return {
         code:gameId,
-        seasons:(msg.seasons && msg.seasons === 4?4:2),
-        seasonLength:(msg.seasonLength && msg.seasonLength >=1 && msg.seasonLength <= 30 ? msg.seasonLength:1),//turn back to 6
-        firstSeason:(msg.firstSeason && msg.firstSeason >=1 && msg.firstSeason <= 30 ? msg.firstSeason:1),//turn back to 12
-        maxPlayers:(msg.maxPlayers && msg.maxPlayers >=4 && msg.maxPlayers <= 7 ? msg.maxPlayers:7),
+        seasons:(msg.longYear && msg.longYear === true?4:2),
+        seasonLength:(msg.seasonLength && msg.seasonLength >=1 && msg.seasonLength <= 30 ? msg.seasonLength:6),
+        firstSeason:(msg.firstSeason && msg.firstSeason >=1 && msg.firstSeason <= 30 ? msg.firstSeason:12),
+        maxPlayers:(msg.maxPlayers && msg.maxPlayers >=2 && msg.maxPlayers <= 7 ? msg.maxPlayers:7),
         GM:(msg.GM && msg.GM === true),
         players:[{name:msg.name.substring(0,30),id:userId}]
     };
